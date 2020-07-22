0b7bc864bd05d8abfd23ced694be7d6e9d2a537c
Poopae50619 <Poopae50619@gmai.com>
2020-07-23 00:48:58 +0700

    folder perpared

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,20 @@
-console.log("Hello")
+let express = require('express')
+const app = express()
+
+app.get('/status', function (req, res){
+    res.send('Hello nodejs server')
+})
+
+let port = 8081
+
+app.listen(port, function () {
+    console.log('Server running on ' + port)
+})
+// get user by id
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน: '+ req.params.userId)
+    })
+// get all user
+app.get('/users', function (req, res) {
+    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
+})
